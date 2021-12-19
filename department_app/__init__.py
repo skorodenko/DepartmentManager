982ebf117163c70255fa5f57905e59b03b94f921
skorodenko <72010957+skorodenko@users.noreply.github.com>
2021-12-19 16:25:30 +0200

    Project structure (#1)
    Finished basic project structure.

---
+++ department_app/__init__.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_mapping(
+    DEBUG=True,
+    SECRET_KEY="dev",
+    SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://rinkuro:1111@localhost/test",
+)
+
+
+db = SQLAlchemy(app)
