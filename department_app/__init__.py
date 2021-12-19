37ebdcfe0eaefc9c4156634a9ca4519d674e89c3
skorodenko <mskorodenko@gmail.com>
2021-12-19 16:19:00 +0200

    Finished basic project structure.

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,25 +1,14 @@
-import os
 
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+app = Flask(__name__)
+app.config.from_mapping(
+    DEBUG=True,
+    SECRET_KEY="dev",
+    SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://rinkuro:1111@localhost/test",
+)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return render_template("employees.html")
-
-    return app
+db = SQLAlchemy(app)
---
+++ department_app/models/__init__.py
@@ -0,0 +1,3 @@
+
+
+from . import department, employee
