6218fa4de6936d7b1d77b3611633c0ee648637c4
skorodenko <mskorodenko@gmail.com>
2022-01-02 17:11:59 +0200

    Improved app creation.

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,5 +1,6 @@
 
 import os
+import secrets
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -8,19 +9,34 @@ from flask_migrate import Migrate
 
 MIGRATIONS_DIRECTORY = os.path.join("department_app", "migrations")
 
-app = Flask(__name__)
-app.config.from_mapping(
+
+def create_app(test_config=None):
+    
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=secrets.token_hex(16)
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.from_object(test_config)
+
+    return app
+
+
+class TEST_CONFIG:
     TESTING=True,
     ENV="development",
     SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://test:1111@localhost/test",
     SQLALCHEMY_TRACK_MODIFICATIONS=True,
-)
+
+app = create_app(TEST_CONFIG)
 
 api = Api(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, directory=MIGRATIONS_DIRECTORY)
-
 
 from .rest import init_rest
 init_rest()
